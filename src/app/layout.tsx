0b3f6d4114ede31e823a7910ff84e800400a2395
wattmi-wattmi <wattmiwattmi@outlook.com>
2025-04-24 03:32:25 +0700

    pwa setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -20,11 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+    <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    >
+    {children}
+    </body>
     </html>
   );
 }
@@ -43,9 +44,26 @@ export const metadata: Metadata = {
     manifest: "/manifest.json",
     appleWebApp: {
         capable: true,
-        statusBarStyle: "default",
+        statusBarStyle: "black-translucent",
         title: APP_DEFAULT_TITLE,
-        // startUpImage: [],
+        startupImage: [
+            '/ios-splash-screens/640x1136.png', // iPhone 5/SE (640x1136)
+            { url: '/ios-splash-screens/375x667.png', media: '(device-width: 375px) and (device-height: 667px)' }, // iPhone 6/7/8
+            { url: '/ios-splash-screens/414x736.png', media: '(device-width: 414px) and (device-height: 736px)' }, // iPhone 6+/7+/8+
+
+            // Notched iPhones (X/XS/11 Pro/12/13/14/15)
+            { url: '/ios-splash-screens/375x812.png', media: '(device-width: 375px) and (device-height: 812px)' }, // iPhone X/XS/11 Pro/12/13 Mini/14
+            { url: '/ios-splash-screens/390x844.png', media: '(device-width: 390px) and (device-height: 844px)' }, // iPhone 12/13/14 (standard)
+            { url: '/ios-splash-screens/428x926.png', media: '(device-width: 428px) and (device-height: 926px)' }, // iPhone 12/13/14 Pro Max
+
+            // iPhone XR/11 (same width as Plus, but taller)
+            { url: '/ios-splash-screens/414x896.png', media: '(device-width: 414px) and (device-height: 896px)' },
+
+            // iPhone 14 Pro/15 (Dynamic Island)
+            { url: '/ios-splash-screens/393x852.png', media: '(device-width: 393px) and (device-height: 852px)' }, // iPhone 14 Pro/15 Pro
+            { url: '/ios-splash-screens/430x932.png', media: '(device-width: 430px) and (device-height: 932px)' },
+            { url: '/ios-splash-screens/768x1024.png', media: '(device-width: 768px) and (device-height: 1024px)' },
+        ],
     },
     formatDetection: {
         telephone: false,
@@ -70,5 +88,11 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-    themeColor: "#FFFFFF",
+    themeColor: "#FF57B4",
+    width: 'device-width',
+    initialScale: 1,
+    minimumScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    viewportFit: 'cover',
 };
