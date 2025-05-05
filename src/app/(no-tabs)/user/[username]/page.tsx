473b330b64bb138ae87d5aba9be94b94c2f5613b
wattmi-wattmi <wattmiwattmi@outlook.com>
2025-05-06 01:50:20 +0700

    user detail page setup

--- src/app/(no-tabs)/user/[username]/page.tsx
+++ src/app/(no-tabs)/user/[username]/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export default function User_Detail_Browse_Page() {
+interface Page_Props_Interface {
+    params: Promise<{ username: string }>;
+}
+
+export default async function User_Detail_Browse_Page({ params } : Page_Props_Interface) {
+    const { username } = await params;
     return (
         <div>
-            <div>Wattmi Wattmi USER DETAIL BROWSE Page</div>
+            <div>Wattmi Wattmi USER DETAIL BROWSE Page : { username }</div>
         </div>
     );
 }
